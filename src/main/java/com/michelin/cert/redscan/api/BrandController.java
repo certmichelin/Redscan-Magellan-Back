c1136be59c7dc9fa0b61485e6c32d1da1ed3451a
axrk <ax.remack@gmail.com>
2022-11-24 17:23:59 +0100

    :art: Fix comments and imports to pass CI

--- src/main/java/com/michelin/cert/redscan/api/BrandController.java
+++ src/main/java/com/michelin/cert/redscan/api/BrandController.java
@@ -22,7 +22,14 @@ import com.michelin.cert.redscan.utils.models.Brand;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -104,8 +111,8 @@ public class BrandController {
   /**
    * Update a brand.
    *
-   * @param name Brand name.
-   * @return True if the entity is updated.
+   * @param brand Brand to update.
+   * @return True if the update succeeded.
    */
   @PutMapping()
   public boolean update(@RequestBody Brand brand) {
